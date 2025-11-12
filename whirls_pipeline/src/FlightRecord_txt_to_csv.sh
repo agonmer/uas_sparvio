970497a3c384d56960415718d39564d965915b54
lfdavoli <l.davoli@campus.unimib.it>
2025-11-12 15:57:38 +0100

    Move dji-log into src.

--- whirls_pipeline/src/FlightRecord_txt_to_csv.sh
+++ whirls_pipeline/src/FlightRecord_txt_to_csv.sh
@@ -3,4 +3,4 @@
 # from https://github.com/lvauvillier/dji-log-parser
 INPUT_FILE=$1
 OUTPUT_FILE=$2
-/home/lorenzo/uas_sparvio/whirls_pipeline/dji-log --api-key 63aada608d30dd4a71fa97cf62006c3 --csv $OUTPUT_FILE $INPUT_FILE
+/home/lorenzo/uas_sparvio/whirls_pipeline/src/dji-log --api-key 63aada608d30dd4a71fa97cf62006c3 --csv $OUTPUT_FILE $INPUT_FILE
